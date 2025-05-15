317a4514906a48910c0b5f384f6ac742b9de55b1
MichelMeloG <michelmg.dev145@gmail.com>
2025-05-14 21:50:16 -0300

    Adiciona configuração do GitHub Actions e ajusta base URL do Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Site-Formula1/', // name of your github repo
 })
